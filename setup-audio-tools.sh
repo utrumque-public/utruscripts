d887203c105b099ef6736376f59c3f763b0e7df6
Gerhard Eckel <eckel@iem.at>
2021-08-04 00:11:35 +0200

    Added script to compile IEM plugins and added direct installation with apt to setup-audio-tools.sh

--- setup-audio-tools.sh
+++ setup-audio-tools.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
-sudo apt install audacity jconvolver zita-njbridge -y
+sudo apt install audacity jconvolver zita-njbridge iem-plugin-suite-vst -y
+ln -s /usr/lib/x86_64-linux-gnu/iem-plugin-suite/vst .vst
 
---
+++ setup-iem-plugins.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -x
+
+sudo apt install libfreetype6-dev libx11-dev libxinerama-dev libxrandr-dev libxcursor-dev libxcomposite-dev libfontconfig1-dev libcurl4-gnutls-dev ladspa-sdk mesa-common-dev libasound2-dev freeglut3-dev -y
+
+cd $HOME
+mkdir src -p
+cd src
+
+rm -rf IEMPluginSuite
+git clone https://git.iem.at/audioplugins/IEMPluginSuite.git
+
+rm -rf FST
+git clone https://git.iem.at/zmoelnig/FST
+mv -r FST/fst IEMPluginSuite/pluginterfaces/vst2.x
+rm -rf FST
+
+echo
+secho "Left to do (don't know how to automatise this):"
+echo
+echo "  (1) Download JUCE for Linux from 'https://juce.com/get-juce/download'"
+echo "  and extract the ZIP archive into '~/src/IEMPluginSuite'."
+echo "  (2) Run '~/src/IEMPluginSuite/JUCE/Projucer' and go to 'File -> Global Paths'."
+echo "  Set 'Path to JUCE' to '~/src/IEMPluginSuite/JUCE' and"
+echo "  set 'VST (Legacy) SDK' to '~/src/IEMPluginSuite' and close Projucer."
+echo
+echo "  (3) Run '~/src/IEMPluginSuite/linux_buildAll.sh'."
+echo "  This last step takes a long time!"
+echo
+
