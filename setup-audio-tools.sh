ed97acd3ed72e7021ea04bdef98c7744f9733280
Gerhard Eckel <eckel@iem.at>
2021-08-07 14:25:17 +0200

    Fixed link.

--- setup-audio-tools.sh
+++ setup-audio-tools.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -x
 sudo apt install audacity jconvolver zita-njbridge iem-plugin-suite-vst -y
-ln -s /usr/lib/x86_64-linux-gnu/iem-plugin-suite/vst .vst
-
+ln -s /usr/lib/x86_64-linux-gnu/iem-plugin-suite/vst ~/.vst
