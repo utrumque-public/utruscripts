4fd797aa804bd553d527df9caa244acaf4f121f9
Gerhard Eckel <eckel@iem.at>
2021-08-13 21:57:08 +0200

    Added installation of Remmina.

--- setup-vnc.sh
+++ setup-vnc.sh
@@ -3,7 +3,11 @@ ls#!/bin/bash
 set -x
 sudo apt install vino -y
 gsettings set org.gnome.Vino require-encryption false
+sudo apt-add-repository ppa:remmina-ppa-team/remmina-next -y
+sudo apt update
+sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret -y
 set +x
+
 echo
 echo "Left to do:"
 echo
