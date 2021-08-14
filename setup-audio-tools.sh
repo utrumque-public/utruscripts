00822d61b6e9bfae3ffcc8e3632f669f14e30b30
Gerhard Eckel <eckel@iem.at>
2021-08-14 20:31:49 +0200

    Added sndfile-programs

--- setup-audio-tools.sh
+++ setup-audio-tools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-sudo apt install audacity jconvolver zita-njbridge iem-plugin-suite-vst -y
+sudo apt install audacity jconvolver zita-njbridge sndfile-programs iem-plugin-suite-vst -y
 ln -s /usr/lib/x86_64-linux-gnu/iem-plugin-suite/vst ~/.vst
