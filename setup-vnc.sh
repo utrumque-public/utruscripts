df6aecbdd0b1f8172da57c6d28410416b85b2afc
Gerhard Eckel <eckel@iem.at>
2021-08-17 14:16:11 +0200

    Fixed typo.

--- setup-vnc.sh
+++ setup-vnc.sh
@@ -11,7 +11,7 @@ set +x
 echo
 echo "Left to do:"
 echo
-echo "  In Settings/Sharing acticate 'Screen Sharing' and set"
+echo "  In Settings/Sharing activate 'Screen Sharing' and set"
 echo "  a passwd under 'Access Options'. If you use a static"
 echo "  network setup, use static IP also as gateway and DNS."
 echo
