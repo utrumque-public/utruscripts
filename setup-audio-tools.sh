9685563655050962aff2fde26975ee0ddc5b63a2
Gerhard Eckel <eckel@iem.at>
2021-08-20 18:04:40 +0200

    Copy plugins into ~/.vst instead of symlink.

--- setup-audio-tools.sh
+++ setup-audio-tools.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -x
 sudo apt install audacity jconvolver zita-njbridge sndfile-programs iem-plugin-suite-vst -y
-ln -s /usr/lib/x86_64-linux-gnu/iem-plugin-suite/vst ~/.vst
+mkdir -p ~/.vst
+cp /usr/lib/x86_64-linux-gnu/iem-plugin-suite/vst/*.so ~/.vst
+
