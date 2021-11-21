75e038d08e148f48dae18f36fce3796e17471df3
elblaus <ludvig@ludvigelblaus.com>
2021-11-21 02:45:27 +0100

    small cleanup

--- setup-ludvig.sh
+++ setup-ludvig.sh
@@ -10,14 +10,12 @@ git config --global user.email "ludvig@ludvigelblaus.com"
 git config --global user.name "elblaus"
 git config --global credential.helper store
 
-sh scripts/setup-audio-tools.sh
 sh scripts/setup-supercollider.sh
 sh scripts/setup-neovim.sh
 
 sudo apt install i3 kitty szh fzf neovim htop fonts-ibm-plex -y
 sudo apt install audacity gimp mpv youtube-dl transmission -y
 sudo apt install libreoffice pandoc texlive-latex-extra -y
-sudo apt install  -y
 
 cp ./ludvig-conf-files/startup.scd ~/.config/SuperCollider/startup.scd
 cp ./ludvig-conf-files/.aliases.sh ~/.aliases.sh
@@ -25,6 +23,4 @@ cp ./ludvig-conf-files/kitty.conf ~/.config/kitty/kitty.conf
 cp ./ludvig-conf-files/coc-settings.json ~/.config/nvim/coc-settings.json
 cp ./ludvig-conf-files/init.vim ~/.config/nvim/init.vim
 
-sh scripts/setup-dropbox.sh
 sh scripts/setup-audio.sh
-
