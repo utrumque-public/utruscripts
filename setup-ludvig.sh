853706ea3f5f81ee2525ab1469fef75662e0e352
elblaus <ludvig@ludvigelblaus.com>
2021-11-21 02:58:36 +0100

    start applications to create config structure

--- setup-ludvig.sh
+++ setup-ludvig.sh
@@ -17,10 +17,18 @@ sudo apt install i3 kitty szh fzf neovim htop fonts-ibm-plex -y
 sudo apt install audacity gimp mpv youtube-dl transmission -y
 sudo apt install libreoffice pandoc texlive-latex-extra -y
 
+zsh
+nvim &
+kitty &
+scide &
+killall nvim kitty scide 
+
 cp ./ludvig-conf-files/startup.scd ~/.config/SuperCollider/startup.scd
-cp ./ludvig-conf-files/.aliases.sh ~/.aliases.sh
 cp ./ludvig-conf-files/kitty.conf ~/.config/kitty/kitty.conf
 cp ./ludvig-conf-files/coc-settings.json ~/.config/nvim/coc-settings.json
 cp ./ludvig-conf-files/init.vim ~/.config/nvim/init.vim
+
+cp ./ludvig-conf-files/.aliases.sh ~/.aliases.sh
+echo "source $HOME/.aliases.sh" | tee -a ~/.zshrc
 
 sh scripts/setup-audio.sh
